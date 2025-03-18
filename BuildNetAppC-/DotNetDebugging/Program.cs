077d5fa1e6023d2be1f99a9f90b94df9c356ebd2
ahamadey27 <hamadey@gmail.com>
2025-03-18 11:25:01 -0400

    >NET Working with Files PT1

--- BuildNetAppC-/DotNetDebugging/Program.cs
+++ BuildNetAppC-/DotNetDebugging/Program.cs
@@ -1,8 +1,14 @@
-﻿int result = Fibonacci(5);
+﻿using System.Diagnostics;
+int result = Fibonacci(6);
 Console.WriteLine(result);
+
 
 static int Fibonacci(int n)
 {
+
+    Debug.WriteLine($"Entering {nameof(Fibonacci)} method");
+    Debug.WriteLine($"We are looking for the {n}th number");
+
     int n1 = 0;
     int n2 = 1;
     int sum;
@@ -12,7 +18,12 @@ static int Fibonacci(int n)
         sum = n1 + n2;
         n1 = n2;
         n2 = sum;
+        Debug.WriteLineIf(sum == 1, $"sum is 1, n1 is {n1}, n2 is {n2}"); //print line only when sum is 1 at the end of the for loop
     }
 
+    Debug.Assert(n2 == 5, "The return value is not 5 and it should be."); //will ternminte application here if condition is not met
     return n == 0 ? n1 : n2;
+
 }
+
+
