730f90dd5f60eb7f9d7850750a6eb4e108574fcb
ahamadey27 <hamadey@gmail.com>
2025-03-19 16:51:58 -0400

    Finished C# PT2 Section 3

---
+++ Csharp_Path_Projects/Part2_SimpleConsole_Apps/ChallangeBusinessRules/ChallangeBusinessRules/Program.cs
@@ -0,0 +1,22 @@
+﻿Random random = new Random();
+
+int daysUntilExperation = random.Next(12); //random number between 0 and 11
+int discountPercentage = 0;
+
+if (daysUntilExperation <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}    
+else if (daysUntilExperation <= 5)
+{
+    Console.WriteLine($"Your subscription will end in {daysUntilExperation} days.\nRenew now and save 10%");
+}
+else if (daysUntilExperation == 1)
+{
+    Console.WriteLine("Your subscription expires within a day!\nRenew now and save 20%!");
+}
+else
+{
+    Console.WriteLine("Subscrition has expired!");
+}
+
---
+++ Csharp_Path_Projects/Part2_SimpleConsole_Apps/DecisionLogic/DecisionLogic/Program.cs
@@ -0,0 +1,35 @@
+﻿Random dice = new Random(); //Create a new instance of the System.Random class and store a reference to the object in a variable
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}.");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll3 == roll1)) //check rolling double condition
+{
+    if ((roll1 == roll3) && (roll2 == roll3)) //Nested if / else statement
+    {
+        Console.WriteLine("You rolled triple three points");
+        total += 6;
+    }
+    else
+    {
+        Console.WriteLine("You rolled double. 2 bonus points.");
+        total += 2;
+    }
+    
+}
+
+if (total >= 15)
+{
+    Console.WriteLine("You win!");
+}
+
+else
+{
+    Console.WriteLine("Sorry you lose");
+}
+
