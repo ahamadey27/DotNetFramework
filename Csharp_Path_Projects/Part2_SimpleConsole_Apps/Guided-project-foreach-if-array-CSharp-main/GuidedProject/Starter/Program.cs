c1aaa54c75a0fa79be372a957f0dfdc5751f8a8a
ahamadey27 <hamadey@gmail.com>
2025-03-20 12:43:37 -0400

    Completed guided project

---
+++ Csharp_Path_Projects/Part2_SimpleConsole_Apps/Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+// initialize variables - graded assignments 
+int currentAssignments = 5;
+
+int[] sophiaScores = { 90, 86, 87, 98, 100 };
+int[] andrewScores = { 92, 89, 81, 96, 90 };
+int[] emmaScores = { 92, 78, 81, 89, 68 };
+int[] loganScores = { 78, 78, 90, 87, 71 };
+
+//Holds student's letter grades
+string currentStudentLetterGrade = "";
+
+//Student names
+string[] studentNames = { "Sophia", "Andrew", "Emma", "Logan" };
+
+int[] studentScores = new int[10];
+
+foreach (string name in studentNames)
+{
+    string currentStudent = name;
+
+    if (currentStudent == "Sophia")
+    {
+        studentScores = sophiaScores;
+    }
+
+    else if (currentStudent == "Andrew")
+    {
+        studentScores = andrewScores;
+    }
+
+    else if (currentStudent == "Emma")
+    {
+        studentScores = emmaScores;
+    }
+
+    else if (currentStudent == "Logan")
+    {
+        studentScores = loganScores;
+    }
+    // initialize/reset the sum of scored assignments
+    int sumAssignmentScores = 0;
+
+    // initialize/reset the calculated average of exam + extra credit scores
+    decimal currentStudentGrade = 0;
+
+    foreach (int score in studentScores)
+    {
+        sumAssignmentScores += score;
+    }
+
+    currentStudentGrade = (decimal)sumAssignmentScores / currentAssignments;
+
+    if (currentStudentGrade >= 97)
+        currentStudentLetterGrade = "A+";
+
+    else if (currentStudentGrade >= 93)
+        currentStudentLetterGrade = "A";
+
+    else if (currentStudentGrade >= 90)
+        currentStudentLetterGrade = "A-";
+
+    else if (currentStudentGrade >= 87)
+        currentStudentLetterGrade = "B+";
+
+    else if (currentStudentGrade >= 83)
+        currentStudentLetterGrade = "B";
+
+    else if (currentStudentGrade >= 80)
+        currentStudentLetterGrade = "B-";
+
+    else if (currentStudentGrade >= 77)
+        currentStudentLetterGrade = "C+";
+
+    else if (currentStudentGrade >= 73)
+        currentStudentLetterGrade = "C";
+
+    else if (currentStudentGrade >= 70)
+        currentStudentLetterGrade = "C-";
+
+    else if (currentStudentGrade >= 67)
+        currentStudentLetterGrade = "D+";
+
+    else if (currentStudentGrade >= 63)
+        currentStudentLetterGrade = "D";
+
+    else if (currentStudentGrade >= 60)
+        currentStudentLetterGrade = "D-";
+
+    else
+        currentStudentLetterGrade = "F";
+
+    //Console.WriteLine("Student\t\tGrade\n");
+    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}");
+}
