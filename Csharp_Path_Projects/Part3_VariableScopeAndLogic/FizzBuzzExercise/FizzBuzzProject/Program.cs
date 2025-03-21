147a71c19297a581961cfad5d67c12e73e7f1d03
ahamadey27 <hamadey@gmail.com>
2025-03-21 14:08:34 -0400

    FizzBuzz Exercise

---
+++ Csharp_Path_Projects/Part3_VariableScopeAndLogic/FizzBuzzExercise/FizzBuzzProject/Program.cs
@@ -0,0 +1,26 @@
+﻿//Classic FizzBuzz.
+//i  divisable by 3 outputs "Fizz"
+//i  divisable by 5 outputs "Buzz"
+//i  divisable by 3 and 5 outputs "FizzBuzz"
+
+
+for (int i = 1; i <= 100; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine("Fizz");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+
+    else
+    {
+        Console.WriteLine(i);
+    }
+}
---
+++ Csharp_Path_Projects/Part3_VariableScopeAndLogic/ForLoopExercise/ForLoopExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿// for (int i = 0; i < 10; i++) //iteration stops at 9
+// {
+//     Console.WriteLine(i);
+//     if (i == 7)
+//     {
+//         break;
+//     }
+// }
+
+//cycle through and output all elements of the length of an array 
+//replace "John" element with "Ryan" condition
+string[] name = { "Alex", "Bill", "John", "Sam" };
+for (int i = 0; i < name.Length; i++)
+{
+    if (name[i] == "John")
+    {
+        name[i] = "Ryan";
+    }
+}
+
+foreach (var names in name)
+{
+    Console.WriteLine(names);
+}
