8c6ec5be6c4a828613b4acc21304c08b55675102
ahamadey27 <hamadey@gmail.com>
2025-03-26 15:03:59 -0400

    Finished Methods using Parameters Module

---
+++ Csharp_Path_Projects/Part5_Methods/DisplayEmailAddressesChallenge/DisplayEmailAddressesChallenge/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Ceate a method that displays the correct email address for both internal and external employees. 
+You're given lists of internal and external employee names. 
+An employee's email address consists of their username and company domain name.*/
+
+string[,] corporate = //first two letters of first name and last name
+{
+    {"Robert", "Bavin"}, {"Simon", "Bright"},
+    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
+};
+
+string[,] external = //first two letters of first name and last name
+{
+    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+};
+
+string externalDomain = "hayworth.com";
+
+
+
+for (int i = 0; i < corporate.GetLength(0); i++)
+{
+    DisplayEmail(first: corporate[i, 0], last: corporate[i, 1]); 
+}
+
+for (int i = 0; i < external.GetLength(0); i++)
+{
+    DisplayEmail(first: external[i, 0], last: external[i, 1], domain: "@hayworth.com");
+}
+
+void DisplayEmail(string first, string last, string domain = "contoso.com")
+{
+    string email = first.Substring(0, 2) + last;
+    email = email.ToLower();
+    Console.WriteLine($"{email}@{domain}");
+}
---
+++ Csharp_Path_Projects/Part5_Methods/VariableScopeExamples/VariableScopeExamples/Program.cs
@@ -0,0 +1,69 @@
+﻿//the method parameter student takes precedence over the global student array.
+string[] students = { "Jenna", "Ayesha", "Carlos", "Viktor" };
+DisplayStudents(students);
+DisplayStudents(new string[] { "Robert", "Vanya" });
+void DisplayStudents(string[] students)
+{
+    foreach (string student in students)
+    {
+        Console.Write($"{student}, ");
+    }
+    Console.WriteLine();
+}
+
+
+//Example of Scpope
+double pi = 3.14159;
+PrintCircleInfo(12);
+PrintCircleInfo(24);
+void PrintCircleInfo(int radius)
+{
+    Console.WriteLine($"Circle with radius {radius}");
+    PrintCircleArea(radius);
+    PrintCircleCircumference(radius);
+}
+void PrintCircleArea(int radius)
+{
+    double area = pi * (radius * radius);
+    Console.WriteLine($"Area = {area}");
+}
+
+void PrintCircleCircumference(int radius)
+{
+    double circumference = 2 * pi * radius;
+    Console.WriteLine($"Circumference = {circumference}");
+}
+
+//Pass arguments by value types
+int a = 3;
+int b = 4;
+int c = 0;
+Multiply(a, b, c);
+Console.WriteLine($"global statement: {a} x {b} = {c}");
+void Multiply(int a, int b, int c)
+{
+    c = a * b;
+    Console.WriteLine($"inside Multiply method: {a} x {b} = {c}");
+}
+
+//Pass arguments by refernce types
+int[] array = { 1, 2, 3, 4, 5 };
+PrintArray(array);
+Clear(array);
+PrintArray(array);
+void PrintArray(int[] array)
+{
+    foreach (int a in array)
+    {
+        Console.Write($"{a} ");
+    }
+    Console.WriteLine();
+}
+
+void Clear(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = 0;
+    }
+}
