cab77073c77a1cfa9c323608769708d0101c382b
ahamadey27 <hamadey@gmail.com>
2025-03-27 17:05:18 -0400

    Unit Test via MSTests Project

---
+++ Csharp_Path_Projects/Part6_TestingAndDebugging/LearnMyCalculatorApp/LearnMyCaulculatorApp.Tests/Test1.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LearnMyCalculatorApp;
+
+namespace LearnMyCaulculatorApp.Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void SubtractTest()
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            var actual = calculator.Subtract(1, 1);
+
+            // Assert
+            Assert.AreEqual(0, actual);
+        }
+
+        [TestMethod]
+        public void MultiplyTest()
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            var actual = calculator.Multiply(1, 1);
+
+            // Assert
+            Assert.AreEqual(1, actual);
+        }
+
+        [TestMethod]
+        public void DivideTest()
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            var actual = calculator.Divide(1, 1);
+
+            // Assert
+            Assert.AreEqual(1, actual);
+        }
+
+        public int? Divide(int x, int y)
+        {
+            try
+            {
+                return x / y;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Cannot divide by zero.");
+                return null;
+            }
+        }
+
+
+        public void CalculatorNullTest()
+        {
+            var calculator = new Calculator();
+            Assert.IsNotNull(calculator);
+            Assert.IsTrue(false);
+        }
+    }
+}
